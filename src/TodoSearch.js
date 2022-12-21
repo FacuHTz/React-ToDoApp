2e6e0bc60102a6e696ddf76e8a2133de5dfd5454
Facundo Hetze <hetzefacundo@gmail.com>
2022-12-21 18:09:25 -0300

    Testing functionalities

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
-  return <input className="TodoSearch" placeholder="find tasks" />;
+
+  const state = React.useState();
+  
+  const onSearchValueChange = (event) =>{
+    console.log(event.target.value);
+  };
+
+  return <input onChange={onSearchValueChange} className="TodoSearch" placeholder="find tasks" />;
 }
 
 export { TodoSearch };
