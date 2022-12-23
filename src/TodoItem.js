07eaf160eb2bc5d5731c5abd81fcc4a95d9a8444
FacuHTz <hetzefacundo@gmail.com>
2022-12-23 08:43:58 -0300

    Adding all from computer

---
+++ src/TodoItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./TodoItem.css";
+
+function TodoItem(props){
+    const onComplete = () => {
+        alert("Ya cimpletaste el TODO " + props.text);
+    };
+
+    return(
+        <li className="TodoItem">
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>✓</span>
+            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
+            <span className="Icon Icon-delete">⊗</span>
+
+        </li>
+    );
+
+}
+
+export {TodoItem};
+
+
